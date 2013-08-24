624ea25ba57d71f123205deb0aacfd0fc03ed99b
Ross Kinney <railspup@gmail.com>
2013-08-23 20:54:30 -0600

    Commit Css changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ChickenCrossing::Application.routes.draw do
 
-   root 'welcome#index'
+  root to: 'static_pages#home'
 
+  #get "static_pages/home"
+  get 'home'       => 'static_pages#home'
+  get 'about'      => 'static_pages#about'
 end
